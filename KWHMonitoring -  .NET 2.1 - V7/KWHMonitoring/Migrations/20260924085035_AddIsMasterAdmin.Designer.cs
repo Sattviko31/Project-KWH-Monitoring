0b224421e3f602f01c779fc2a762d0192ec6f456
Alfin Faiz Febrianto <alvinf889@gmail.com>
2026-09-24 16:17:09 +0700

    Fix migration AddIsMasterAdmin dan penyesuaian skema RelayControl

--- KWHMonitoring -  .NET 2.1 - V7/KWHMonitoring/Migrations/20260924085035_AddIsMasterAdmin.Designer.cs
+++ KWHMonitoring -  .NET 2.1 - V7/KWHMonitoring/Migrations/20260924085035_AddIsMasterAdmin.Designer.cs
@@ -4,14 +4,16 @@ using KWHMonitoring.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace KWHMonitoring.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260924085035_AddIsMasterAdmin")]
+    partial class AddIsMasterAdmin
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
